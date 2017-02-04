bdca288543f3c9a55c2a315b448b21f1f64ec1f8
Josh Malmquist <joshmalmquist96@gmail.com>
2017-02-03 23:37:44 -0500

    Intital Flight mode for parrot
    The intital idea for the parrots flight mode has been prototyped.

--- Assets/Scripts/Parrot.cs
+++ Assets/Scripts/Parrot.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 public class Parrot : MonoBehaviour {
     #region Attributes
     private int health = 10;
-    private float speed = 5.0f;
+    [SerializeField] private float speed = 2.0f;
+    [SerializeField] private float turnSpeed = 2.0f;
+    private float currentHeight;
+    
+    private Rigidbody rBody;
+
+    //input stuff
+    private float inputDelay = 0.3f;
+    private float horizontalInput = 0;
+    private float verticalInput = 0;
+    private float camXInput = 0;
+    private float camYInput = 0;
     #endregion
 
     #region Properties
@@ -13,18 +24,62 @@ public class Parrot : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
+        rBody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        ParrotMove();
+        
 	}
 
     #region Methods
     private void ParrotMove()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        camXInput = Input.GetAxis("Cam_Horizontal");
+        //moves parrot constantly forward
+        rBody.velocity = transform.forward * speed;
+        
+        if(Mathf.Abs(horizontalInput) > inputDelay)
+        {
+            //if input is greater than 0;
+            if(horizontalInput > 0)
+            {
+                transform.Rotate(new Vector3(0, turnSpeed, 0));
+            }
+            else if(horizontalInput < 0)
+            {
+                transform.Rotate(new Vector3(0, -turnSpeed, 0));
+            }
+        }
 
+        if(Mathf.Abs(verticalInput) > inputDelay)
+        {
+            if(verticalInput > 0)
+            {
+                transform.Rotate(new Vector3(turnSpeed, 0, 0));
+            }
+            else if(verticalInput < 0)
+            {
+                transform.Rotate(new Vector3(-turnSpeed, 0, 0));
+            }
+        }
+        print(rBody.velocity);
+
+        if(Mathf.Abs(camXInput) > inputDelay)
+        {
+            if (camXInput > 0)
+            {
+                transform.Rotate(new Vector3(0, 0, turnSpeed));
+            }
+            else
+            {
+                transform.Rotate(new Vector3(0, 0, -turnSpeed));
+            }
+        }
+        
     }
     #endregion
 }
