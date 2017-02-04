61a72888f03ffa68d0c2c06f1767b7acb59e0059
Charles Williams <crw4147@rit.edu>
2017-02-03 23:35:52 -0500

    made test scene

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// class to govern the behaviour of NPC's when they are not under the player's control
+/// </summary>
+public class NPC : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
