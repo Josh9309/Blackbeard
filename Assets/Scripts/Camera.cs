00f2c2e51ecbb330ddc4752ff58045d48a4ca52a
Josh Malmquist <joshmalmquist96@gmail.com>
2017-02-06 00:33:04 -0500

    Parrot Flight Mode V2
    I have gone through and updated the parrot flight movement. The parrot
    will now move forwards when the left analog stick is pushed forwards.
    The parrot will turn when you move the left analog stick left or right.
    if parrot is turning while moving then the parrot will angle in the
    direction it is turning. The parrot can now fly up or down by using the
    triggers. the left trigger flies up and the right trigger will fly down.
    The parrot will angle in the correct direction when flying up or down.
    
    The 3rd person camera has now been updated. The camera is now under a
    camera pivot empty game object. when the camera is rotate left or right
    then x axis rotation is zeroed and vice versa.
    
    Both the camera and the parrot have prefabs now.

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    #region Attributes
+    public GameObject target;
+    private float camXInput = 0;
+    private float camYInput = 0;
+
+    private float inputDelay = 0.3f;
+
+    [SerializeField] private float rotSpeed = 1f;
+    #endregion
+
+    #region Properties
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        transform.position = target.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = target.transform.position;
+
+        camXInput = Input.GetAxis("Cam_Horizontal");
+        camYInput = Input.GetAxis("Cam_Vertical");
+
+        if (Mathf.Abs(camXInput) > inputDelay)
+        {
+            transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+            if (camXInput > 0)
+            {
+                transform.Rotate(0, rotSpeed, 0);
+            }
+            else
+            {
+                transform.Rotate(0, -rotSpeed, 0);
+            }
+        }
+        else if (Mathf.Abs(camYInput) > inputDelay)
+        {
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, 0, 0);
+            if (camYInput > 0)
+            {
+                transform.Rotate(rotSpeed, 0, 0);
+            }
+            else
+            {
+                transform.Rotate(-rotSpeed, 0, 0);
+            }
+        }
+    }
+}
--- Assets/Scripts/Parrot.cs
+++ Assets/Scripts/Parrot.cs
@@ -10,13 +10,14 @@ public class Parrot : MonoBehaviour {
     private float currentHeight;
     
     private Rigidbody rBody;
-
+    private bool rotateParrot = false;
+    Vector3 parrotRotation = new Vector3(); //parrot euler angle rotation
     //input stuff
     private float inputDelay = 0.3f;
     private float horizontalInput = 0;
     private float verticalInput = 0;
-    private float camXInput = 0;
-    private float camYInput = 0;
+    private float flyUpInput = 0;
+    private float flyDownInput = 0;
     #endregion
 
     #region Properties
@@ -30,28 +31,73 @@ public class Parrot : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         ParrotMove();
-        
+
+        //parrot Rotation
+        //if (rotateParrot) //if parrot needs to be rotated
+        //{
+        //    if(Vector3.Distance(transform.localRotation.eulerAngles, parrotRotation) > 0.01f)
+        //    {
+        //        transform.eulerAngles = Vector3.Lerp(transform.localRotation.eulerAngles, parrotRotation, Time.deltaTime);
+        //    }
+        //    else
+        //    {
+        //        transform.eulerAngles = parrotRotation;
+        //        rotateParrot = false;
+        //    }
+        //}
 	}
 
     #region Methods
     private void ParrotMove()
     {
+
+        //Get inputs for Parrot movement
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-        camXInput = Input.GetAxis("Cam_Horizontal");
-        //moves parrot constantly forward
-        rBody.velocity = transform.forward * speed;
         
-        if(Mathf.Abs(horizontalInput) > inputDelay)
+        flyUpInput = Input.GetAxis("FlyUp");
+        flyDownInput = Input.GetAxis("FlyDown");
+
+        //zero velocity
+        rBody.velocity = Vector3.zero;
+
+        //zero parrot rotation
+        parrotRotation = Vector3.zero;
+
+        
+        
+        //Parrot fly up
+        if(Mathf.Abs(flyUpInput) > inputDelay)
+        {
+            rBody.velocity += new Vector3(0, speed, 0);
+            parrotRotation += new Vector3(-15, 0, 0);
+            rotateParrot = true;
+        }
+        else if (Mathf.Abs(flyDownInput) > inputDelay) //parrot fly down
+        {
+            rBody.velocity += new Vector3(0, -speed, 0);
+            parrotRotation += new Vector3(15, 0, 0);
+        }
+        else
+        {
+            rotateParrot = true;
+            parrotRotation = Vector3.zero;
+        }
+
+        if (Mathf.Abs(horizontalInput) > inputDelay)
         {
             //if input is greater than 0;
             if(horizontalInput > 0)
             {
                 transform.Rotate(new Vector3(0, turnSpeed, 0));
+                parrotRotation.y = transform.localEulerAngles.y;
+               // parrotRotation.z = -45;
             }
             else if(horizontalInput < 0)
             {
                 transform.Rotate(new Vector3(0, -turnSpeed, 0));
+                parrotRotation.y = transform.localEulerAngles.y;
+               // parrotRotation.z = 45;
             }
         }
 
@@ -59,27 +105,29 @@ public class Parrot : MonoBehaviour {
         {
             if(verticalInput > 0)
             {
-                transform.Rotate(new Vector3(turnSpeed, 0, 0));
+                //moves parrot constantly forward
+                rBody.velocity += transform.forward * speed;
             }
-            else if(verticalInput < 0)
+            if (Mathf.Abs(horizontalInput) > inputDelay)
             {
-                transform.Rotate(new Vector3(-turnSpeed, 0, 0));
+                //if input is greater than 0;
+                if (horizontalInput > 0)
+                {
+                    parrotRotation.z = -45;
+                }
+                else if (horizontalInput < 0)
+                {
+                    parrotRotation.z = 45;
+                }
             }
         }
+
         print(rBody.velocity);
 
-        if(Mathf.Abs(camXInput) > inputDelay)
-        {
-            if (camXInput > 0)
-            {
-                transform.Rotate(new Vector3(0, 0, turnSpeed));
-            }
-            else
-            {
-                transform.Rotate(new Vector3(0, 0, -turnSpeed));
-            }
-        }
-        
+        parrotRotation.y = transform.localEulerAngles.y;
+
+
+        transform.localEulerAngles = parrotRotation;
     }
     #endregion
 }
