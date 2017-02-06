2706ca9782468c973d1d9cc2898672d6bcaf66c3
Kevin Idzik <kdi4293@rit.edu>
2017-02-06 13:40:19 -0500

    Parrot Flight Experimentation and Gizmo

---
+++ Assets/Scripts/ParrotCopter.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParrotCopter : MonoBehaviour
+{
+    #region Attributes
+    //Parrot
+    private int health = 10;
+    [SerializeField] private float speed = 2.0f;
+    [SerializeField] private float maxSpeed = 5.0f;
+    private float savedMaxSpeed;
+    [SerializeField] private float turnSpeed = 5.0f;
+    [SerializeField] private float minHeight = 0;
+    [SerializeField] private float maxHeight = 15;
+    private float currentHeight;    
+    private Rigidbody rBody;
+    private float flapCycleTime;
+
+    //Input
+    private float deadZone = .1f;
+
+    //input stuff
+    //private float inputDelay = 0.3f;
+    //private float horizontalInput = 0;
+    //private float verticalInput = 0;
+    //private float camXInput = 0;
+    //private float camYInput = 0;
+    #endregion
+
+    #region Properties
+    #endregion
+
+    void Start() //Use this for initialization
+    {
+        rBody = GetComponent<Rigidbody>();
+        savedMaxSpeed = maxSpeed;
+	}
+	
+	void Update() //Update is called once per frame
+    {
+
+	}
+
+    private void FixedUpdate() //Physics updates
+    {
+        ParrotControl();
+    }
+
+    private void ParrotControl()
+    {
+        //Make the parrot fly forward and backwards
+        if (Input.GetAxis("Vertical") > deadZone)
+        {
+            rBody.velocity += transform.forward * (speed / 2);
+            rBody.velocity = Vector3.ClampMagnitude(rBody.velocity, maxSpeed);
+        }
+        else if (Input.GetAxis("Vertical") < -deadZone)
+        {
+            rBody.velocity = -transform.forward;
+        }
+        else
+        {
+            rBody.velocity = Vector3.zero;
+        }
+
+        //Make the parrot bank left and right
+        if (Input.GetAxis("Horizontal") > deadZone)
+        {
+            transform.Rotate(0, turnSpeed, 0);
+        }
+        else if (Input.GetAxis("Horizontal") < -deadZone)
+        {
+            transform.Rotate(0, -turnSpeed, 0);
+        }
+        else
+        {
+            transform.Rotate(0, 0, 0);
+        }
+
+        //Make the parrot fly upwards
+        if (Input.GetAxis("FlyUp") > deadZone && transform.position.y < maxHeight)
+        {
+            rBody.velocity += transform.up * Mathf.Lerp(maxSpeed, speed, flapCycleTime);
+
+            flapCycleTime += 2.5f * Time.deltaTime;
+
+            if (flapCycleTime > 1f)
+            {
+                flapCycleTime = 0f;
+            }
+        }
+
+        //Make the parrot fly downwards
+        if (Input.GetAxis("FlyDown") > deadZone && transform.position.y > minHeight)
+        {
+            rBody.velocity -= transform.up * Mathf.Lerp(maxSpeed, speed, flapCycleTime);
+
+            flapCycleTime += 2.5f * Time.deltaTime;
+
+            if (flapCycleTime > 1f)
+            {
+                flapCycleTime = 0f;
+            }
+        }
+
+        //BOOST
+        if (Input.GetButton("BoostFly"))
+        {
+            maxSpeed = savedMaxSpeed * 2;
+        }
+        else
+        {
+            maxSpeed = savedMaxSpeed;
+        }
+
+        Debug.Log(rBody.velocity.y);
+    }
+}
