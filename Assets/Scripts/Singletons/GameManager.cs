9ffd26fb791f2c43a6c4660490f9c581ec9cae24
Joel Shuart <jas1785@g.rit.edu>
2017-02-08 00:03:44 -0500

    singletons added, as well as models and enimations folders
    sound and menu managers are coming

---
+++ Assets/Scripts/Singletons/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Handles main play cycle logic. Anything within game manager should happen during gameplay
+/// and terminate when advancing to other menus.
+/// </summary>
+public class GameManager : Singleton<GameManager>
+{
+    #region Fields
+    //Assigned in inspector
+    private List<GameObject> pirates = new List<GameObject>();
+    private GameObject player; 
+    #endregion
+
+    #region Properties
+    #endregion
+
+    protected GameManager() { }
+
+    void Awake()
+    {
+        //start game
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        //MenuManager.Instance.GoToScreen("GameScreen");
+
+    }
+
+
+
+    void Update()
+    {
+
+    }
+}
