398831e9fd69a03306a12c8e4b62d4b55f81508b
Josh Malmquist <joshmalmquist96@gmail.com>
2017-02-09 23:21:48 -0500

    Revert "Started Basic AI"
    This reverts commit 2146ef351e74d56f6ce418b180563a4305baff8c.

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -4,24 +4,16 @@ using UnityEngine;
 
 /// <summary>
 /// class to govern the behaviour of NPC's when they are not under the player's control
-/// this class is abstract and has two children, RedPirate and BluePirate
 /// </summary>
-public abstract class NPC : MonoBehaviour {
-    // for use in identifying what team the pirate is on
-    protected string team;
-
-    // all components of the pirate
-    protected Rigidbody rb;
-
-    // accessors
+public class NPC : MonoBehaviour {
 
 	// Use this for initialization
-	protected virtual void Start () {
-        rb = this.GetComponent<Rigidbody>();
+	void Start () {
+
 	}
 	
 	// Update is called once per frame
-	protected virtual void Update () {
+	void Update () {
 		
 	}
 }
