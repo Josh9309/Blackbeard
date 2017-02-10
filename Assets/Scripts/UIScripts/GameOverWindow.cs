fc2e91c4bfe384f4146234411b549263e3a7145c
Josh Malmquist <joshmalmquist96@gmail.com>
2017-02-09 23:27:16 -0500

    Bad commit fix
    This fix resolves the last of the errors that occured because of a bad
    git merge that corrupted the assets.

--- Assets/Scripts/UIScripts/GameOverWindow.cs
+++ Assets/Scripts/UIScripts/GameOverWindow.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class GameOverWindow : BaseWindow
 {
     #region Attributes
-    MenuManager manager;
+   // MenuManager manager;
     #endregion
 
     #region Properties
