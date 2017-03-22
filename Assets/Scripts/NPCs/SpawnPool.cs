4fb281c349b7c24790be38d612e1fec93167973e
Josh Malmquist <joshmalmquist96@gmail.com>
2017-03-22 13:19:32 -0400

    added Treasure Pirate model + Began SpawnPools
    I have added the Treasure Hunter Pirate model and created the material
    for it. I removed the reduant lamberts for the other models. I have also
    begun work on creating the spawn pool prefabs.

---
+++ Assets/Scripts/NPCs/SpawnPool.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPool : MonoBehaviour {
+    #region Attributes
+    [SerializeField] private GameManager.Team pirateTeam;
+    private List<GameObject> squads;
+    private List<GameObject> orphans;
+    [SerializeField] private float spawnRadius = 15;
+    private float spawnDelay = 10;
+    private bool canSpawn;
+    private int MaxSquads = 2;
+    private Queue spawnRequests;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    #region Helper Methods
+    private void SpawnSquad()
+    {
+
+    }
+    #endregion
+}
