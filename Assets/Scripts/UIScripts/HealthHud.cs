a2c68319ec3a360e2ff5d5890e0d5544441a1d4f
Satch Puri <sp1796@g.rit.edu>
2017-03-24 02:22:13 -0400

    Added Treasure Meter, Fixed HealthBar
    will clean up code soon

--- Assets/Scripts/UIScripts/HealthHud.cs
+++ Assets/Scripts/UIScripts/HealthHud.cs
@@ -4,38 +4,30 @@ using UnityEngine;
 
 public class HealthHud : MonoBehaviour
 {
-    NPC health;
+    MeleeNPC health;
     //max health
-    public float totalHealth;
-    public float currentHealth;
-
+   //public float totalHealth;
+    float currentHealth;
     public GameObject healthBar;
 	// Use this for initialization
 	void Start ()
     {
-        health = GetComponent<NPC>();
-        totalHealth = health.Health;
+        health = GetComponent<MeleeNPC>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         currentHealth = health.Health;
-
-        InvokeRepeating("decreaseHealth", .1f, .1f);
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
-
-    void decreaseHealth()
-    {
-       // currentHealth -= 2f;
-        float calcHealth = currentHealth / totalHealth;
-        SetHealthbar(calcHealth);
+        // Debug.Log(currentHealth);
+        // decreaseHealth();
+        SetHealthbar(currentHealth / 150.0f);
     }
 
     void SetHealthbar(float healthbar)
     {
         //healthbar needs to be between 0-1
         healthBar.transform.localScale = new Vector3(healthbar, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+       // Debug.Log(healthbar);
     }
 }
---
+++ Assets/Scripts/UIScripts/TreasureMeter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureMeter : MonoBehaviour
+{
+    [SerializeField]
+    GameObject redBar;
+    [SerializeField]
+    GameObject blueBar;
+    [SerializeField]
+    GameObject treasure;
+    [SerializeField]
+    GameObject redTreasureLoc;
+    [SerializeField]
+    GameObject blueTreasureLoc;
+    float currentdistRed;
+    float totalDistRed;
+    float currentDistBlue;
+    float totalDistBlue;
+
+    // Use this for initialization
+    void Start ()
+    {
+        totalDistRed = Vector3.Distance(treasure.transform.position, redTreasureLoc.transform.position);
+        totalDistBlue = Vector3.Distance(treasure.transform.position, blueTreasureLoc.transform.position);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currentdistRed = Vector3.Distance(treasure.transform.position, redTreasureLoc.transform.position);
+        currentDistBlue = Vector3.Distance(treasure.transform.position, blueTreasureLoc.transform.position);
+
+        if (treasure.transform.position != redTreasureLoc.transform.position)
+        {
+            if (redBar.transform.localScale.x <= 1)
+            {
+                redBar.transform.localScale = new Vector3(currentdistRed / totalDistRed, redBar.transform.localScale.y, redBar.transform.localScale.z);
+            }
+        }
+
+        if (treasure.transform.position != blueTreasureLoc.transform.position)
+        {
+            if (blueBar.transform.localScale.x <= 1)
+            {
+                blueBar.transform.localScale = new Vector3(currentDistBlue / totalDistBlue, blueBar.transform.localScale.y, blueBar.transform.localScale.z);
+            }
+         }
+    }
+}
