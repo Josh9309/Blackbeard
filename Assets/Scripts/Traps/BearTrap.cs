a3a61e2146ba020226ef5b3e12dfe56dfc3bba66
Charles Williams <charlesrw96@yahoo.com>
2017-03-28 23:12:42 -0400

    Simple Bear Trap

---
+++ Assets/Scripts/Traps/BearTrap.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearTrap : BaseTrap {
+
+    #region Attributes
+    [SerializeField]
+    private float stunTime;
+    #endregion
+
+    // Use this for initialization
+    protected override void Start () {
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+        base.Update();
+	}
+
+    public override void Activate()
+    {
+        Debug.Log("Bear trap activated");
+        activated = true;       
+    }
+
+    public override void Deactivate()
+    {
+        activated = false;
+        trapAnim.Play("Deactivate");
+    }
+
+    public override void Reset()
+    {
+        activated = false;
+        triggered = false;
+        trapAnim.Play("Deactivate");
+    }
+
+    public override void Trigger(GameObject pirate)
+    {
+        triggered = true;
+        trapAnim.Play("Activate");
+        Debug.Log("Bear trap triggered");
+        StartCoroutine(pirate.GetComponent<CaptainPirate>().Stun(stunTime));
+    }
+}
