6dc7b2e2befdcbfb8590f6ccf3fa8068a8756215
Satch Puri <sp1796@g.rit.edu>
2017-04-14 01:50:12 -0400

    Hud Interaction stuff

---
+++ Assets/Scripts/UIScripts/HudInteraction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HudInteraction : MonoBehaviour {
+
+    [SerializeField] GameObject parrot1;
+    [SerializeField] GameObject parrot2;
+    [SerializeField] GameObject trap;
+    [SerializeField] Canvas HUD;
+	// Use this for initialization
+	void Start ()
+    {
+        HUD.GetComponent<Canvas>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 if((Vector3.Distance(parrot1.transform.position, trap.transform.position) < 10) || (Vector3.Distance(parrot2.transform.position, trap.transform.position) < 10))
+            {
+            HUD.GetComponent<Canvas>().enabled = true;
+        }
+	}
+}
