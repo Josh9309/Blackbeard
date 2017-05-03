ebf7e4d429923a4d952b0a7b5ca0fe831553f5fa
jayshuart <joelshuart@gmail.com>
2017-05-03 01:25:53 -0400

    compass functionality
    needs better model, and on/off player input

---
+++ Assets/Scripts/UIScripts/Compass.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour {
+    #region Fields
+    [SerializeField] private GameObject treasure;
+    #endregion
+
+    #region Start and Update
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FaceTreasure();
+	}
+    #endregion
+
+    #region Helper Methods
+    private void FaceTreasure()
+    {
+        //look at treasure
+        this.gameObject.transform.LookAt(treasure.transform);
+
+    }
+    #endregion
+}
