24bb39e3736640bc4b7fcb9f5d464de75414203c
jayshuart <joelshuart@gmail.com>
2017-05-05 21:19:25 -0400

    new timer fun

--- Assets/Scripts/UIScripts/Timer.cs
+++ Assets/Scripts/UIScripts/Timer.cs
@@ -8,12 +8,17 @@ public class Timer : MonoBehaviour {
     GameManager gm;
 
     public Text timer;
+    public Text bigTime;
+    public Text bigTime2;
+
 	// Use this for initialization
 	void Start () {
         gm = GameManager.Instance;
 	}
 
 	void Update () {
+        bigTime.text = " ";
+        bigTime2.text = " ";
 
         timer.color = Color.white;
         timer.text = "" + gm.CurrentPhaseTime;
@@ -23,6 +28,16 @@ public class Timer : MonoBehaviour {
             timer.text = " " + gm.CurrentPhaseTime;
             timer.color = Color.red;
             timer.fontSize = 80;
+
+            bigTime.text = " " + gm.CurrentPhaseTime;
+            bigTime.color = new Color32(0, 0, 0, 100);
+            bigTime.fontSize = 300;
+
+            bigTime2.text = " " + gm.CurrentPhaseTime;
+            bigTime2.color = new Color32(0, 0, 0, 100);;
+            bigTime2.fontSize = 300;
+
+
         }
     }
 }
